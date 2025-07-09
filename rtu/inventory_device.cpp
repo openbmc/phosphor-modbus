0f5d74b2d52ce8b8207061e56b94ba0d8f7d352e
Jagpal Singh Gill <paligill@gmail.com>
2025-07-09 00:31:43 -0700

    implement modbus rtu inventory source service
    Implement phosphor-modbus-rtu inventory source service based on [1].
    
    [1]: https://gerrit.openbmc.org/c/openbmc/docs/+/77318
    
    Tested: Unit test passes and tested on qemu with simulated modbus
    server.
    
    Change-Id: Ic0ea739de3833044c95da8164be1e2f3f8e6a063
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ rtu/inventory_device.cpp
@@ -0,0 +1,421 @@
+#include "inventory_device.hpp"
+
+#include "common/entity_manager_interface.hpp"
+
+#include <boost/container/flat_map.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <xyz/openbmc_project/Configuration/ModbusRTUDetect/client.hpp>
+#include <xyz/openbmc_project/Inventory/Item/client.hpp>
+
+namespace phosphor::modbus::rtu::inventory
+{
+PHOSPHOR_LOG2_USING;
+
+namespace config
+{
+
+using BasicVariantType =
+    std::variant<std::vector<std::string>, std::vector<uint8_t>, std::string,
+                 int64_t, uint64_t, double, int32_t, uint32_t, int16_t,
+                 uint16_t, uint8_t, bool>;
+using InventoryBaseConfigMap =
+    boost::container::flat_map<std::string, BasicVariantType>;
+using InventoryData =
+    boost::container::flat_map<std::string, InventoryBaseConfigMap>;
+using ManagedObjectType =
+    boost::container::flat_map<sdbusplus::message::object_path, InventoryData>;
+
+static constexpr std::array<std::pair<std::string_view, Parity>, 3>
+    validParities = {
+        {{"Odd", Parity::odd}, {"Even", Parity::even}, {"None", Parity::none}}};
+
+// TODO: This API will be dropped once EM supports non-indexed interfaces for
+// array objects.
+static auto processModbusAddressInterface(
+    Config& config, const InventoryBaseConfigMap& configMap) -> bool
+{
+    debug("Processing ModbusAddress {NAME}", "NAME", config.name);
+
+    auto rangeStartIter = configMap.find("RangeStart");
+    if (rangeStartIter == configMap.end())
+    {
+        error("Missing RangeStart for {NAME}", "NAME", config.name);
+        return false;
+    }
+    auto rangeStart = std::get<uint64_t>(rangeStartIter->second);
+
+    auto rangeEndIter = configMap.find("RangeEnd");
+    if (rangeEndIter == configMap.end())
+    {
+        error("Missing RangeEnd for {NAME}", "NAME", config.name);
+        return false;
+    }
+    auto rangeEnd = std::get<uint64_t>(rangeEndIter->second);
+
+    auto serialPortIter = configMap.find("SerialPort");
+    if (serialPortIter == configMap.end())
+    {
+        error("Missing SerialPort for {NAME}", "NAME", config.name);
+        return false;
+    }
+    auto serialPort = std::get<std::string>(serialPortIter->second);
+
+    config.addressMap[serialPort].push_back({rangeStart, rangeEnd});
+
+    debug("ModbusAddress {NAME} {PORT} {START} {END}", "NAME", config.name,
+          "PORT", serialPort, "START", rangeStart, "END", rangeEnd);
+
+    return true;
+}
+
+// TODO: This API will be dropped once EM supports non-indexed interfaces for
+// array objects.
+static auto processModbusRegistersInterface(
+    Config& config, const InventoryBaseConfigMap& configMap) -> bool
+{
+    debug("Processing ModbusRegisters {NAME}", "NAME", config.name);
+    Register registerConfig = {};
+
+    auto nameIter = configMap.find("Name");
+    if (nameIter == configMap.end())
+    {
+        error("Missing Name for {NAME}", "NAME", config.name);
+        return false;
+    }
+    registerConfig.name = std::get<std::string>(nameIter->second);
+
+    auto address = configMap.find("Address");
+    if (address == configMap.end())
+    {
+        error("Missing Address for {NAME}", "NAME", config.name);
+        return false;
+    }
+    registerConfig.offset = std::get<uint64_t>(address->second);
+
+    auto sizeIter = configMap.find("Size");
+    if (sizeIter == configMap.end())
+    {
+        error("Missing Size for {NAME}", "NAME", config.name);
+        return false;
+    }
+    registerConfig.size = std::get<uint64_t>(sizeIter->second);
+
+    config.registers.push_back(registerConfig);
+
+    debug("ModbusRegisters {NAME} {ADDRESS} {SIZE}", "NAME",
+          registerConfig.name, "ADDRESS", registerConfig.offset, "SIZE",
+          registerConfig.size);
+
+    return true;
+}
+
+static auto printConfig(const Config& config) -> void
+{
+    info("Inventory device config: {NAME} {BAUDRATE} {PARITY}", "NAME",
+         config.name, "BAUDRATE", config.baudRate, "PARITY", config.parity);
+
+    for (const auto& [port, addressRanges] : config.addressMap)
+    {
+        for (const auto& addressRange : addressRanges)
+        {
+            info(
+                "Inventory device config: {PORT} {ADDRESS_START} {ADDRESS_END}",
+                "PORT", port, "ADDRESS_START", std::get<0>(addressRange),
+                "ADDRESS_END", std::get<1>(addressRange));
+        }
+    }
+
+    for (const auto& registerConfig : config.registers)
+    {
+        info("Inventory device config: {NAME} {ADDRESS} {SIZE}", "NAME",
+             registerConfig.name, "ADDRESS", registerConfig.offset, "SIZE",
+             registerConfig.size);
+    }
+}
+
+auto getConfigSubInterfaces(sdbusplus::async::context& ctx,
+                            sdbusplus::message::object_path objectPath,
+                            Config& config) -> sdbusplus::async::task<bool>
+{
+    constexpr auto modbusAddressInterface =
+        "xyz.openbmc_project.Configuration.ModbusRTUDetect.Address";
+    constexpr auto modbusRegistersInterface =
+        "xyz.openbmc_project.Configuration.ModbusRTUDetect.Registers";
+
+    using InventoryIntf =
+        sdbusplus::client::xyz::openbmc_project::inventory::Item<>;
+
+    constexpr auto entityManager =
+        sdbusplus::async::proxy()
+            .service(entity_manager::EntityManagerInterface::serviceName)
+            .path(InventoryIntf::namespace_path)
+            .interface("org.freedesktop.DBus.ObjectManager");
+
+    for (const auto& [path, deviceConfig] :
+         co_await entityManager.call<ManagedObjectType>(ctx,
+                                                        "GetManagedObjects"))
+    {
+        if (!(path.str).starts_with(objectPath.str))
+        {
+            debug("Skipping device {PATH}", "PATH", path.str);
+            continue;
+        }
+        debug("Processing device {PATH}", "PATH", path.str);
+        for (const auto& [interfaceName, interfaceConfig] : deviceConfig)
+        {
+            if (interfaceName.starts_with(modbusAddressInterface))
+            {
+                if (!processModbusAddressInterface(config, interfaceConfig))
+                {
+                    error("Failed to process {INTERFACE} for {NAME}",
+                          "INTERFACE", modbusAddressInterface, "NAME",
+                          config.name);
+                    co_return false;
+                }
+            }
+            else if (interfaceName.starts_with(modbusRegistersInterface))
+            {
+                if (!processModbusRegistersInterface(config, interfaceConfig))
+                {
+                    error("Failed to process {INTERFACE} for {NAME}",
+                          "INTERFACE", modbusRegistersInterface, "NAME",
+                          config.name);
+                    co_return false;
+                }
+            }
+        }
+    }
+
+    co_return true;
+}
+
+auto getConfig(sdbusplus::async::context& ctx,
+               sdbusplus::message::object_path objectPath)
+    -> sdbusplus::async::task<std::optional<Config>>
+{
+    using ModbusRTUDetectIntf = sdbusplus::client::xyz::openbmc_project::
+        configuration::ModbusRTUDetect<>;
+
+    Config config = {};
+
+    auto properties =
+        co_await ModbusRTUDetectIntf(ctx)
+            .service(entity_manager::EntityManagerInterface::serviceName)
+            .path(objectPath.str)
+            .properties();
+
+    config.name = properties.name;
+    config.baudRate = properties.baud_rate;
+
+    for (const auto& [parityStr, parity] : config::validParities)
+    {
+        if (parityStr == properties.data_parity)
+        {
+            config.parity = parity;
+            break;
+        }
+    }
+    if (config.parity == Parity::unknown)
+    {
+        error("Invalid parity {PARITY} for {NAME}", "PARITY",
+              properties.data_parity, "NAME", properties.name);
+        co_return std::nullopt;
+    }
+
+    if (!co_await getConfigSubInterfaces(ctx, objectPath, config))
+    {
+        co_return std::nullopt;
+    }
+
+    printConfig(config);
+
+    co_return config;
+}
+
+} // namespace config
+
+Device::Device(sdbusplus::async::context& ctx, const config::Config& config,
+               serial_port_map_t& serialPorts) :
+    ctx(ctx), config(config), serialPorts(serialPorts)
+{
+    for (const auto& [serialPort, addressRanges] : config.addressMap)
+    {
+        if (serialPorts.find(serialPort) == serialPorts.end())
+        {
+            throw std::runtime_error("Serial port not found");
+        }
+    }
+}
+
+auto Device::probePorts() -> sdbusplus::async::task<void>
+{
+    debug("Probing ports for {NAME}", "NAME", config.name);
+    while (!ctx.stop_requested())
+    {
+        for (const auto& [serialPort, addressRanges] : config.addressMap)
+        {
+            ctx.spawn(probePort(serialPort));
+        }
+        constexpr auto probeInterval = 3;
+        co_await sdbusplus::async::sleep_for(
+            ctx, std::chrono::seconds(probeInterval));
+        debug("Probing ports for {NAME} in {INTERVAL} seconds", "NAME",
+              config.name, "INTERVAL", probeInterval);
+    }
+}
+
+auto Device::probePort(std::string portName) -> sdbusplus::async::task<void>
+{
+    debug("Probing port {PORT}", "PORT", portName);
+
+    auto portConfig = config.addressMap.find(portName);
+    if (portConfig == config.addressMap.end())
+    {
+        error("Serial port {PORT} address map not found for {NAME}", "PORT",
+              portName, "NAME", config.name);
+        co_return;
+    }
+    auto addressRanges = portConfig->second;
+
+    auto port = serialPorts.find(portName);
+    if (port == serialPorts.end())
+    {
+        error("Serial port {PORT} not found for {NAME}", "PORT", portName,
+              "NAME", config.name);
+        co_return;
+    }
+
+    for (const auto& addressRange : addressRanges)
+    {
+        auto rangeStart = std::get<0>(addressRange);
+        auto rangeEnd = std::get<1>(addressRange);
+        for (auto address = rangeStart; address <= rangeEnd; address++)
+        {
+            co_await probeDevice(address, portName, port->second);
+        }
+    }
+}
+
+auto Device::probeDevice(uint8_t address, const std::string& portName,
+                         const std::unique_ptr<SerialPortIntf>& port)
+    -> sdbusplus::async::task<void>
+{
+    debug("Probing device at {ADDRESS} on port {PORT}", "ADDRESS", address,
+          "PORT", portName);
+
+    if (config.registers.size() == 0)
+    {
+        error("No registers configured for {NAME}", "NAME", config.name);
+        co_return;
+    }
+    auto probeRegister = config.registers[0].offset;
+    auto registers = std::vector<uint16_t>(config.registers[0].size);
+
+    auto ret = co_await port->readHoldingRegisters(
+        address, probeRegister, config.baudRate, config.parity, registers);
+    if (ret)
+    {
+        if (inventorySources.find(address) == inventorySources.end())
+        {
+            debug("Device found at {ADDRESS}", "ADDRESS", address);
+            co_await addInventorySource(address, portName, port);
+        }
+        else
+        {
+            debug("Device already exists at {ADDRESS}", "ADDRESS", address);
+        }
+    }
+    else
+    {
+        if (inventorySources.find(address) != inventorySources.end())
+        {
+            debug(
+                "Device removed at {ADDRESS} due to probe failure for {PROBE_REGISTER}",
+                "ADDRESS", address, "PROBE_REGISTER", probeRegister);
+            inventorySources[address]->emit_removed();
+            inventorySources.erase(address);
+        }
+    }
+}
+
+static auto fillInventorySourceProperties(
+    InventorySourceIntf::properties_t& properties, const std::string& regName,
+    std::string& strValue) -> void
+{
+    constexpr auto partNumber = "PartNumber";
+    constexpr auto sparePartNumber = "SparePartNumber";
+    constexpr auto serialNumber = "SerialNumber";
+    constexpr auto buildDate = "BuildDate";
+    constexpr auto model = "Model";
+    constexpr auto manufacturer = "Manufacturer";
+
+    if (regName == partNumber)
+    {
+        properties.part_number = strValue;
+    }
+    else if (regName == sparePartNumber)
+    {
+        properties.spare_part_number = strValue;
+    }
+    else if (regName == serialNumber)
+    {
+        properties.serial_number = strValue;
+    }
+    else if (regName == buildDate)
+    {
+        properties.build_date = strValue;
+    }
+    else if (regName == model)
+    {
+        properties.model = strValue;
+    }
+    else if (regName == manufacturer)
+    {
+        properties.manufacturer = strValue;
+    }
+}
+
+auto Device::addInventorySource(uint8_t address, const std::string& portName,
+                                const std::unique_ptr<SerialPortIntf>& port)
+    -> sdbusplus::async::task<void>
+{
+    InventorySourceIntf::properties_t properties;
+
+    for (const auto& reg : config.registers)
+    {
+        auto registers = std::vector<uint16_t>(reg.size);
+        auto ret = co_await port->readHoldingRegisters(
+            address, reg.offset, config.baudRate, config.parity, registers);
+        if (!ret)
+        {
+            error(
+                "Failed to read holding registers {NAME} for {DEVICE_ADDRESS}",
+                "NAME", reg.name, "DEVICE_ADDRESS", address);
+            continue;
+        }
+
+        std::string strValue = "";
+
+        for (const auto& value : registers)
+        {
+            strValue += static_cast<char>((value >> 8) & 0xFF);
+            strValue += static_cast<char>(value & 0xFF);
+        }
+
+        fillInventorySourceProperties(properties, reg.name, strValue);
+    }
+
+    auto objectPath =
+        std::string(InventorySourceIntf::namespace_path) + "/" + config.name +
+        "_" + std::to_string(address) + "_" + portName;
+
+    properties.name = config.name;
+    properties.address = address;
+    properties.link_tty = portName;
+
+    inventorySources[address] = std::make_unique<InventorySourceIntf>(
+        ctx, objectPath.c_str(), properties);
+    inventorySources[address]->emit_added();
+}
+
+} // namespace phosphor::modbus::rtu::inventory
---
+++ tests/modbus_test_server.cpp
@@ -0,0 +1,129 @@
+#include "modbus/modbus.hpp"
+#include "modbus/modbus_commands.hpp"
+#include "modbus/modbus_message.hpp"
+
+#include <fcntl.h>
+
+#include <sdbusplus/async.hpp>
+
+#include <iostream>
+
+namespace phosphor::modbus::test
+{
+
+class MessageIntf : public phosphor::modbus::rtu::Message
+{
+    friend class TestServer;
+};
+
+class TestServer
+{
+  public:
+    explicit TestServer(sdbusplus::async::context& ctx, int fd);
+
+  private:
+    auto processRequests() -> sdbusplus::async::task<void>;
+    void processMessage(MessageIntf& request, size_t requestSize,
+                        MessageIntf& response);
+
+    void processReadHoldingRegisters(MessageIntf& request, size_t requestSize,
+                                     MessageIntf& response);
+
+    sdbusplus::async::context& ctx;
+    int fd;
+    sdbusplus::async::fdio fdioInstance;
+};
+
+TestServer::TestServer(sdbusplus::async::context& ctx, int fd) :
+    ctx(ctx), fd(fd), fdioInstance(ctx, fd)
+{
+    ctx.spawn(processRequests());
+}
+
+auto TestServer::processRequests() -> sdbusplus::async::task<void>
+{
+    while (!ctx.stop_requested())
+    {
+        MessageIntf request;
+        co_await fdioInstance.next();
+        auto ret = read(fd, request.raw.data(), request.raw.size());
+        // Request message need to be at least 4 bytes long - address(1),
+        // function code(1), ..., CRC(2)
+        if (ret < 4)
+        {
+            std::cerr << "Invalid Server message size:" << ret << ", drop it"
+                      << std::endl;
+            continue;
+        }
+
+        MessageIntf response;
+        processMessage(request, ret, response);
+
+        ret = write(fd, response.raw.data(), response.len);
+        if (ret < 0)
+        {
+            std::cerr << "Failed to send response" << std::endl;
+        }
+    }
+}
+
+void TestServer::processMessage(MessageIntf& request, size_t requestSize,
+                                MessageIntf& response)
+{
+    constexpr uint8_t readHoldingRegistersFunctionCode = 0x3;
+
+    switch (request.functionCode)
+    {
+        case readHoldingRegistersFunctionCode:
+            processReadHoldingRegisters(request, requestSize, response);
+            break;
+        default:
+            std::cerr << "Server received unknown request" << std::endl;
+            break;
+    }
+}
+
+void TestServer::processReadHoldingRegisters(
+    MessageIntf& request, size_t requestSize, MessageIntf& response)
+{
+    uint16_t registerOffset = request.raw[2] << 8 | request.raw[3];
+    uint16_t registerCount = request.raw[4] << 8 | request.raw[5];
+
+    std::cout << "Received readHoldingRegisters request with size:"
+              << requestSize << ", registerOffset:" << registerOffset
+              << ", registerCount:" << registerCount << std::endl;
+
+    response << request.raw[0] << request.raw[1] << uint8_t(2 * registerCount);
+    for (int i = 0; i < registerCount; i++)
+    {
+        response << uint16_t(0x4142);
+    }
+    response.appendCRC();
+}
+
+} // namespace phosphor::modbus::test
+
+int main()
+{
+    using TestServerIntf = phosphor::modbus::test::TestServer;
+    static constexpr const char* serverDevicePath = "/dev/ttyV1";
+    constexpr auto path = "/xyz/openbmc_project";
+    constexpr auto serviceName = "xyz.openbmc_project.ModbusRTUTestServer";
+    sdbusplus::async::context ctx;
+
+    auto fdServer = open(serverDevicePath, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (fdServer == -1)
+    {
+        std::cerr << "Failed to open serial port " << serverDevicePath
+                  << " with error: " << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    std::cout << "Creating Modbus RTU test server at " << path << std::endl;
+    TestServerIntf server{ctx, fdServer};
+
+    ctx.request_name(serviceName);
+
+    ctx.run();
+    return 0;
+}
