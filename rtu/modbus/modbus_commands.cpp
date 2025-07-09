36e101bf00e8148ff021a3bcfae123a0b060463c
Jagpal Singh Gill <paligill@gmail.com>
2025-07-09 00:11:26 -0700

    implement modbus rtu library
    Implement modbus-rtu library which will be used by phosphor-modbus-rtu
    service based on [1].
    
    [1]: https://gerrit.openbmc.org/c/openbmc/docs/+/77318
    
    Tested: Unit test passed.
    ```
    ninja: Entering directory `/host/repos/Modbus/phosphor-modbus/builddir'
    ninja: no work to do.
    1/1 test_modbus        OK              4.02s
    
    Ok:                 1
    Expected Fail:      0
    Fail:               0
    Unexpected Pass:    0
    Skipped:            0
    Timeout:            0
    ```
    
    Change-Id: I66cdc8fd930dd6f7ad6888116d1419ad8f8b8ed8
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ rtu/modbus/modbus_commands.cpp
@@ -0,0 +1,42 @@
+#include "modbus_commands.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+namespace phosphor::modbus::rtu
+{
+
+ReadHoldingRegistersRequest::ReadHoldingRegistersRequest(
+    uint8_t deviceAddress, uint16_t registerOffset, uint16_t registerCount) :
+    deviceAddress(deviceAddress), registerOffset(registerOffset),
+    registerCount(registerCount)
+{}
+
+auto ReadHoldingRegistersRequest::encode() -> void
+{
+    *this << deviceAddress << commandCode << registerOffset << registerCount;
+    appendCRC();
+}
+
+ReadHoldingRegistersResponse::ReadHoldingRegistersResponse(
+    uint8_t deviceAddress, std::vector<uint16_t>& registers) :
+    expectedDeviceAddress(deviceAddress), registers(registers)
+{
+    if (registers.empty())
+    {
+        throw std::underflow_error("Response registers are empty");
+    }
+    // addr(1), func(1), bytecount(1), <2 * count regs>, crc(2)
+    len = 5 + (2 * registers.size());
+}
+
+auto ReadHoldingRegistersResponse::decode() -> void
+{
+    validate();
+    uint8_t byteCount, responseCode, deviceAddress;
+    *this >> registers >> byteCount >> responseCode >> deviceAddress;
+    verifyValue("Device Address", deviceAddress, expectedDeviceAddress);
+    verifyValue("Response Function Code", responseCode, expectedCommandCode);
+    verifyValue("Byte Count", byteCount, registers.size() * 2);
+}
+
+} // namespace phosphor::modbus::rtu
---
+++ rtu/modbus/modbus_commands.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "modbus_message.hpp"
+
+#include <vector>
+
+namespace phosphor::modbus::rtu
+{
+
+static constexpr uint8_t ReadHoldingRegistersFunctionCode = 0x03;
+
+class ReadHoldingRegistersRequest : public Message
+{
+  public:
+    ReadHoldingRegistersRequest() = delete;
+    ReadHoldingRegistersRequest(const ReadHoldingRegistersRequest&) = delete;
+    ReadHoldingRegistersRequest& operator=(const ReadHoldingRegistersRequest&) =
+        delete;
+    ReadHoldingRegistersRequest(ReadHoldingRegistersRequest&&) = delete;
+    ReadHoldingRegistersRequest& operator=(ReadHoldingRegistersRequest&&) =
+        delete;
+
+    explicit ReadHoldingRegistersRequest(
+        uint8_t deviceAddress, uint16_t registerOffset, uint16_t registerCount);
+
+    auto encode() -> void;
+
+  private:
+    static constexpr uint8_t commandCode = ReadHoldingRegistersFunctionCode;
+    const uint8_t deviceAddress;
+    const uint16_t registerOffset;
+    const uint16_t registerCount;
+};
+
+class ReadHoldingRegistersResponse : public Message
+{
+  public:
+    ReadHoldingRegistersResponse() = delete;
+    ReadHoldingRegistersResponse(const ReadHoldingRegistersResponse&) = delete;
+    ReadHoldingRegistersResponse& operator=(
+        const ReadHoldingRegistersResponse&) = delete;
+    ReadHoldingRegistersResponse(ReadHoldingRegistersResponse&&) = delete;
+    ReadHoldingRegistersResponse& operator=(ReadHoldingRegistersResponse&&) =
+        delete;
+
+    explicit ReadHoldingRegistersResponse(uint8_t deviceAddress,
+                                          std::vector<uint16_t>& registers);
+
+    auto decode() -> void;
+
+  private:
+    static constexpr uint8_t expectedCommandCode =
+        ReadHoldingRegistersFunctionCode;
+    const uint8_t expectedDeviceAddress;
+    // The returned response is stored in the registers vector
+    std::vector<uint16_t>& registers;
+};
+
+} // namespace phosphor::modbus::rtu
