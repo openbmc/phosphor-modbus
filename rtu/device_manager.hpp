128900ec6d72ed31c9ab124fdd05972ce09dde4f
Jagpal Singh Gill <paligill@gmail.com>
2025-09-01 16:49:08 -0700

    implement modbus sensor and status register read
    Read the modbus device config from the Entity Manager configuration and
    create the sensor interfaces for related sensor register config. For
    status registers raise appropriate events based on status bits in status
    register.
    
    Change-Id: I1368e8df5999b5cee9ac19d185ee110a9ecc3021
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- rtu/device_manager.hpp
+++ rtu/device_manager.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "common/entity_manager_interface.hpp"
+#include "common/events.hpp"
+#include "device.hpp"
 #include "inventory_device.hpp"
 #include "port/base_port.hpp"
 
@@ -11,6 +13,9 @@ namespace phosphor::modbus::rtu
 
 namespace InventoryIntf = phosphor::modbus::rtu::inventory;
 namespace PortIntf = phosphor::modbus::rtu::port;
+namespace ModbusIntf = phosphor::modbus::rtu;
+namespace DeviceIntf = phosphor::modbus::rtu::device;
+namespace EventIntf = phosphor::modbus::events;
 
 class DeviceManager
 {
@@ -26,10 +31,29 @@ class DeviceManager
   private:
     using inventory_device_map_t =
         std::unordered_map<std::string, std::unique_ptr<InventoryIntf::Device>>;
+
     using port_map_t =
         std::unordered_map<std::string, std::unique_ptr<PortIntf::BasePort>>;
 
+    using device_info_t =
+        std::pair<ModbusIntf::Parity, uint32_t>; // < Parity, BaudRate >
+
+    using device_map_t =
+        std::unordered_map<std::string, std::unique_ptr<DeviceIntf::Device>>;
+
     auto processConfigAdded(const sdbusplus::message::object_path& objectPath,
+                            const std::string& interfaceName)
+        -> sdbusplus::async::task<>;
+
+    auto processPortAdded(const sdbusplus::message::object_path& objectPath,
+                          const std::string& interfaceName)
+        -> sdbusplus::async::task<>;
+
+    auto processInventoryAdded(
+        const sdbusplus::message::object_path& objectPath)
+        -> sdbusplus::async::task<>;
+
+    auto processDeviceAdded(const sdbusplus::message::object_path& objectPath,
                             const std::string& interfaceName)
         -> sdbusplus::async::task<>;
 
@@ -37,10 +61,16 @@ class DeviceManager
                               const std::string& interfaceName)
         -> sdbusplus::async::task<>;
 
+    auto getDeviceInfoFromInventory(const std::string& deviceName,
+                                    uint8_t address, std::string& portName)
+        -> std::optional<device_info_t>;
+
     sdbusplus::async::context& ctx;
     entity_manager::EntityManagerInterface entityManager;
+    EventIntf::Events events;
     inventory_device_map_t inventoryDevices;
     port_map_t ports;
+    device_map_t devices; // Modbus devices
 };
 
 } // namespace phosphor::modbus::rtu
