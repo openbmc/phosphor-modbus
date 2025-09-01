44471bef85a7dc990ca6560d82d9ab3a8a01f338
Jagpal Singh Gill <paligill@gmail.com>
2025-09-01 16:49:08 -0700

    implement modbus sensor and status register read
    Read the modbus device config from the Entity Manager configuration and
    create the sensor interfaces for related sensor register config. For
    status registers raise appropriate events based on status bits in status
    register.
    
    Change-Id: I1368e8df5999b5cee9ac19d185ee110a9ecc3021
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- rtu/device_manager.cpp
+++ rtu/device_manager.cpp
@@ -15,15 +15,19 @@ using USBPortConfigIntf =
     sdbusplus::client::xyz::openbmc_project::configuration::USBPort<>;
 using ModbusRTUDetectIntf =
     sdbusplus::client::xyz::openbmc_project::configuration::ModbusRTUDetect<>;
+namespace DeviceConfigIntf = phosphor::modbus::rtu::device::config;
 
 const entity_manager::interface_list_t interfaces{
-    USBPortConfigIntf::interface, ModbusRTUDetectIntf::interface};
+    USBPortConfigIntf::interface, ModbusRTUDetectIntf::interface,
+    DeviceConfigIntf::ModbusRDF040DSS5193E0ReservoirPumpUnitInterface,
+    DeviceConfigIntf::ModbusRDF040DSS5193E0HeatExchangerInterface};
 
 DeviceManager::DeviceManager(sdbusplus::async::context& ctx) :
     ctx(ctx),
     entityManager(ctx, interfaces,
                   std::bind_front(&DeviceManager::processConfigAdded, this),
-                  std::bind_front(&DeviceManager::processConfigRemoved, this))
+                  std::bind_front(&DeviceManager::processConfigRemoved, this)),
+    events(ctx)
 {
     ctx.spawn(entityManager.handleInventoryGet());
     info("DeviceManager created successfully");
@@ -45,49 +49,115 @@ auto DeviceManager::processConfigAdded(
 
     if (interfaceName == USBPortConfigIntf::interface)
     {
-        auto res = co_await SerialPortIntf::config::getConfig(ctx, objectPath);
-        if (!res)
-        {
-            error("Failed to get Serial Port config for {PATH}", "PATH",
-                  objectPath);
-            co_return;
-        }
-        auto config = res.value();
-        try
-        {
-            serialPorts[config.name] =
-                std::make_unique<SerialPortIntf::SerialPort>(ctx, config);
-        }
-        catch (const std::exception& e)
-        {
-            error("Failed to create Serial Port for {PATH} with {ERROR}",
-                  "PATH", objectPath, "ERROR", e);
-            co_return;
-        }
+        co_await processPortAdded(objectPath);
     }
     else if (interfaceName == ModbusRTUDetectIntf::interface)
     {
-        auto res = co_await InventoryIntf::config::getConfig(ctx, objectPath);
-        if (!res)
+        co_await processInventoryAdded(objectPath);
+    }
+    else if (interfaceName ==
+                 DeviceConfigIntf::
+                     ModbusRDF040DSS5193E0ReservoirPumpUnitInterface ||
+             interfaceName ==
+                 DeviceConfigIntf::ModbusRDF040DSS5193E0HeatExchangerInterface)
+    {
+        co_await processDeviceAdded(objectPath, interfaceName);
+    }
+}
+
+auto DeviceManager::processPortAdded(
+    const sdbusplus::message::object_path& objectPath)
+    -> sdbusplus::async::task<>
+{
+    auto res = co_await SerialPortIntf::config::getConfig(ctx, objectPath);
+    if (!res)
+    {
+        error("Failed to get Serial Port config for {PATH}", "PATH",
+              objectPath);
+        co_return;
+    }
+    auto config = res.value();
+    try
+    {
+        serialPorts[config.name] =
+            std::make_unique<SerialPortIntf::SerialPort>(ctx, config);
+    }
+    catch (const std::exception& e)
+    {
+        error("Failed to create Serial Port for {PATH} with {ERROR}", "PATH",
+              objectPath, "ERROR", e);
+        co_return;
+    }
+}
+
+auto DeviceManager::processInventoryAdded(
+    const sdbusplus::message::object_path& objectPath)
+    -> sdbusplus::async::task<>
+{
+    auto res = co_await InventoryIntf::config::getConfig(ctx, objectPath);
+    if (!res)
+    {
+        error("Failed to get Inventory Device config for {PATH}", "PATH",
+              objectPath);
+        co_return;
+    }
+    auto config = res.value();
+    try
+    {
+        auto inventoryDevice =
+            std::make_unique<InventoryIntf::Device>(ctx, config, serialPorts);
+        ctx.spawn(inventoryDevice->probePorts());
+        inventoryDevices[config.name] = std::move(inventoryDevice);
+    }
+    catch (const std::exception& e)
+    {
+        error("Failed to create Inventory Device for {PATH} with {ERROR}",
+              "PATH", objectPath, "ERROR", e);
+        co_return;
+    }
+}
+
+auto DeviceManager::processDeviceAdded(
+    const sdbusplus::message::object_path& objectPath,
+    const std::string& interfaceName) -> sdbusplus::async::task<>
+{
+    auto res =
+        co_await DeviceConfigIntf::getConfig(ctx, objectPath, interfaceName);
+    if (!res)
+    {
+        error("Failed to get Device config for {PATH}", "PATH", objectPath);
+        co_return;
+    }
+    auto config = res.value();
+
+    auto deviceInfo = getDeviceInfoFromInventory(config.name, config.address,
+                                                 config.portName);
+    if (!deviceInfo)
+    {
+        error("Failed to get Device info for {PATH}", "PATH", objectPath);
+        co_return;
+    }
+
+    try
+    {
+        auto serialPort = serialPorts.find(config.portName);
+        if (serialPort == serialPorts.end())
         {
-            error("Failed to get Inventory Device config for {PATH}", "PATH",
-                  objectPath);
+            error("Failed to find Serial Port for device creation for {PATH}",
+                  "PATH", objectPath);
             co_return;
         }
-        auto config = res.value();
-        try
-        {
-            auto inventoryDevice = std::make_unique<InventoryIntf::Device>(
-                ctx, config, serialPorts);
-            ctx.spawn(inventoryDevice->probePorts());
-            inventoryDevices[config.name] = std::move(inventoryDevice);
-        }
-        catch (const std::exception& e)
-        {
-            error("Failed to create Inventory Device for {PATH} with {ERROR}",
-                  "PATH", objectPath, "ERROR", e);
-            co_return;
-        }
+        config.parity = deviceInfo.value().first;
+        config.baudRate = deviceInfo.value().second;
+        auto device = std::make_unique<DeviceIntf::Device>(
+            ctx, config, serialPort->second, events);
+        devices[config.name] = std::move(device);
+    }
+    catch (const std::exception& e)
+    {
+        error("Failed to create Device for {PATH} with {ERROR}", "PATH",
+              objectPath, "ERROR", e);
+        co_return;
     }
 }
 
@@ -97,6 +167,31 @@ auto DeviceManager::processConfigRemoved(
 {
     // TODO: Implement this
     co_return;
+}
+
+auto DeviceManager::getDeviceInfoFromInventory(
+    const std::string& deviceName, uint8_t address, std::string& portName)
+    -> std::optional<device_info_t>
+{
+    std::string deviceSuffix = "_" + std::to_string(address) + "_" + portName;
+    size_t pos = deviceName.find(deviceSuffix);
+
+    if (pos == std::string::npos)
+    {
+        return std::nullopt;
+    }
+
+    std::string inventoryName = deviceName.substr(0, pos);
+
+    const auto& inventoryDevice = inventoryDevices.find(inventoryName);
+
+    if (inventoryDevice == inventoryDevices.end())
+    {
+        return std::nullopt;
+    }
+
+    return device_info_t{inventoryDevice->second->config.parity,
+                         inventoryDevice->second->config.baudRate};
 }
 
 } // namespace phosphor::modbus::rtu
