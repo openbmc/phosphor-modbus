80b5304d2a698287c20e25baeb66adf722327180
Jagpal Singh Gill <paligill@gmail.com>
2025-09-02 15:19:29 -0700

    add firmware inventory support
    Add support to display the firmware version using the Firmware version
    register from EM configuration. The version will be exposed as
    xyz.openbmc_project.Software.Version interface on the Dbus.
    
    Change-Id: I985e12ef88547585cca93569b083f347e74a8695
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ rtu/firmware/device_firmware.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "port/base_port.hpp"
+
+#include <sdbusplus/async.hpp>
+#include <sdbusplus/async/server.hpp>
+#include <xyz/openbmc_project/Association/Definitions/aserver.hpp>
+#include <xyz/openbmc_project/Software/Activation/aserver.hpp>
+#include <xyz/openbmc_project/Software/Version/aserver.hpp>
+
+namespace phosphor::modbus::rtu::device
+{
+
+namespace config
+{
+
+struct Config;
+
+} // namespace config
+
+namespace config_intf = phosphor::modbus::rtu::device::config;
+using PortIntf = phosphor::modbus::rtu::port::BasePort;
+
+class DeviceFirmware;
+
+using FirmwareIntf = sdbusplus::async::server_t<
+    DeviceFirmware, sdbusplus::aserver::xyz::openbmc_project::software::Version,
+    sdbusplus::aserver::xyz::openbmc_project::software::Activation,
+    sdbusplus::aserver::xyz::openbmc_project::association::Definitions>;
+
+class DeviceFirmware : public FirmwareIntf
+{
+  public:
+    DeviceFirmware() = delete;
+
+    explicit DeviceFirmware(sdbusplus::async::context& ctx,
+                            const config_intf::Config& config,
+                            const std::unique_ptr<PortIntf>& serialPort);
+
+  private:
+    auto readVersionRegister() -> sdbusplus::async::task<void>;
+
+    sdbusplus::async::context& ctx;
+    const config_intf::Config& config;
+    const std::unique_ptr<PortIntf>& serialPort;
+};
+
+} // namespace phosphor::modbus::rtu::device
