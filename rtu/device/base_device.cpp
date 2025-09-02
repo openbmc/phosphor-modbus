586cc97974d74cc2505bca8a247807d65a198b77
Jagpal Singh Gill <paligill@gmail.com>
2025-09-02 15:19:29 -0700

    add firmware inventory support
    Add support to display the firmware version using the Firmware version
    register from EM configuration. The version will be exposed as
    xyz.openbmc_project.Software.Version interface on the Dbus.
    
    Change-Id: I985e12ef88547585cca93569b083f347e74a8695
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- rtu/device/base_device.cpp
+++ rtu/device/base_device.cpp
@@ -503,6 +503,7 @@ BaseDevice::BaseDevice(sdbusplus::async::context& ctx,
                        const config::Config& config, PortIntf& serialPort,
                        EventIntf::Events& events) :
     ctx(ctx), config(config), serialPort(serialPort), events(events)
+    currentFirmware(std::make_unique<DeviceFirmware>(ctx, config, serialPort))
 {
     info("Successfully created device {NAME}", "NAME", config.name);
 }
