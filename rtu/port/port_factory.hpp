20063fcbdf2b8253cf5c5f53c7db061153bfb2d7
Jagpal Singh Gill <paligill@gmail.com>
2025-09-29 16:45:23 -0700

    rtu: add serial port and entity manager interface
    Add the interface classes for serial port with Port Factory classes to
    make the code extensible for future in case a new hardware port type is
    introduced. This also makes the unit testing easy by creating a Mock
    Port using socat. Also define an EntityManager interface to fetch the
    serial port related config or any device config from Entity Manager.
    
    Tested:
    ```
    meson test -C builddir test_port
    ninja: Entering directory `/host/repos/Modbus/phosphor-modbus/builddir'
    ninja: no work to do.
    1/1 test_port        OK              5.02s
    
    Ok:                1
    Fail:              0
    ```
    
    Change-Id: Ic6bd982abf1ae993f76c39e3503d3a0402a692fe
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ rtu/port/port_factory.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "base_port.hpp"
+
+#include <sdbusplus/async.hpp>
+
+#include <string>
+#include <vector>
+
+namespace phosphor::modbus::rtu::port
+{
+
+namespace config
+{
+
+enum class PortType
+{
+    usb,
+    unknown
+};
+
+struct PortFactoryConfig : public Config
+{
+    PortType portType = PortType::unknown;
+};
+
+} // namespace config
+
+class PortFactory
+{
+  public:
+    static auto getInterfaces() -> std::vector<std::string>;
+
+    static auto getConfig(sdbusplus::async::context& ctx,
+                          const sdbusplus::message::object_path& objectPath,
+                          const std::string& interfaceName)
+        -> sdbusplus::async::task<std::optional<config::PortFactoryConfig>>;
+
+    static auto create(sdbusplus::async::context& ctx,
+                       config::PortFactoryConfig& config)
+        -> std::unique_ptr<BasePort>;
+};
+
+} // namespace phosphor::modbus::rtu::port
