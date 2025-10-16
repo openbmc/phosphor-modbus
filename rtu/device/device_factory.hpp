181c407bdb7c7a46b204df0bf0af98cf624c471a
Jagpal Singh Gill <paligill@gmail.com>
2025-10-16 00:00:13 -0700

    rtu: implement modbus sensor read
    Read the modbus device config from the Entity Manager configuration and
    create the sensor interfaces for related sensor register config.
    
    Change-Id: I1368e8df5999b5cee9ac19d185ee110a9ecc3021
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ rtu/device/device_factory.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "base_device.hpp"
+
+namespace phosphor::modbus::rtu::device
+{
+
+namespace config
+{
+
+enum class DeviceType
+{
+    reservoirPumpUnit,
+    heatExchanger,
+    flowMeter,
+    unknown
+};
+
+enum class DeviceModel
+{
+    RDF040DSS5193E0,
+    unknown
+};
+
+struct DeviceFactoryConfig : public Config
+{
+    DeviceType deviceType = DeviceType::unknown;
+    DeviceModel deviceModel = DeviceModel::unknown;
+};
+
+} // namespace config
+
+class DeviceFactory
+{
+  public:
+    DeviceFactory() = delete;
+
+    static auto getInterfaces() -> std::vector<std::string>;
+
+    static auto getConfig(sdbusplus::async::context& ctx,
+                          const sdbusplus::message::object_path& objectPath,
+                          const std::string& interfaceName)
+        -> sdbusplus::async::task<std::optional<config::DeviceFactoryConfig>>;
+
+    static auto create(sdbusplus::async::context& ctx,
+                       const config::DeviceFactoryConfig& config,
+                       PortIntf& serialPort) -> std::unique_ptr<BaseDevice>;
+};
+
+} // namespace phosphor::modbus::rtu::device
