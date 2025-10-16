47c26858348030b028ef2d02c0aa13cfb391df49
Jagpal Singh Gill <paligill@gmail.com>
2025-10-16 00:28:58 -0700

    rtu: add read status register and event support
    Add support to read the status registers and generate the appropriate
    events.
    
    Tested:
    ```
    > meson test -t 10 -C builddir/ --print-errorlogs --wrapper="valgrind --error-exitcode=1" test_events
    ninja: Entering directory `/host/repos/Modbus/phosphor-modbus/builddir'
    ninja: no work to do.
    1/1 test_events        OK               9.69s
    
    Ok:                1
    Fail:              0
    ```
    
    Change-Id: Icd78f22cf07798d06916cc077ec3f8bfac9ee8d3
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- rtu/device/base_device.hpp
+++ rtu/device/base_device.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "base_config.hpp"
+#include "common/events.hpp"
 #include "modbus/modbus.hpp"
 #include "port/base_port.hpp"
 
@@ -15,6 +16,7 @@ class Device;
 using SensorValueIntf =
     sdbusplus::aserver::xyz::openbmc_project::sensor::Value<Device>;
 using PortIntf = phosphor::modbus::rtu::port::BasePort;
+namespace EventIntf = phosphor::modbus::events;
 
 class BaseDevice
 {
@@ -22,18 +24,27 @@ class BaseDevice
     BaseDevice() = delete;
 
     explicit BaseDevice(sdbusplus::async::context& ctx,
-                        const config::Config& config, PortIntf& serialPort);
+                        const config::Config& config, PortIntf& serialPort,
+                        EventIntf::Events& events);
 
     auto readSensorRegisters() -> sdbusplus::async::task<void>;
 
   private:
     auto createSensors() -> void;
 
+    auto readStatusRegisters() -> sdbusplus::async::task<void>;
+
+    auto generateEvent(const config::StatusBit& statusBit,
+                       const sdbusplus::message::object_path& objectPath,
+                       double sensorValue, SensorValueIntf::Unit sensorUnit,
+                       bool statusAsserted) -> sdbusplus::async::task<void>;
+
     using sensors_map_t =
         std::unordered_map<std::string, std::unique_ptr<SensorValueIntf>>;
     sdbusplus::async::context& ctx;
     const config::Config config;
     PortIntf& serialPort;
+    EventIntf::Events& events;
     sensors_map_t sensors;
 };
 
