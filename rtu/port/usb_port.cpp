33bb8eeeed834fdca1218866704ad974e942d02f
Amithash Prasad <amithash@meta.com>
2026-03-03 10:16:03 -0800

    rtu: usb: Add support for USB serial devices on a USB Hub
    Add capability for entity manager configuration to provide
    information on the Hub port where the USB device is connected to.
    This would default to 0 (connected to a USB controller). Use
    this information to craft an appropriate regex.
    
    Tested: This was tested on a ventura device (RS485-4) is on a
    USB Hub while RS485-1,2,3 are directly connected to the USB
    controller.
    
    Change-Id: I52d7ddbc1929ba675db64dffc894de569f694c55
    Signed-off-by: Amithash Prasad <amithash@meta.com>

--- rtu/port/usb_port.cpp
+++ rtu/port/usb_port.cpp
@@ -29,6 +29,7 @@ struct USBPortConfig : public PortFactoryConfig
     std::string address = "unknown";
     uint16_t port = 0;
     uint16_t interface = 0;
+    uint16_t hubport = 0;
 
     virtual ~USBPortConfig() = default;
 };
@@ -41,9 +42,26 @@ static auto getDevicePath(const config::PortFactoryConfig& inConfig)
     namespace fs = std::filesystem;
     auto config = static_cast<const config::USBPortConfig&>(inConfig);
 
-    std::regex pattern(
-        std::format("platform-{}\\.usb-usb.*{}-port{}", config.address,
-                    config.interface, config.port));
+    std::string spattern;
+
+    info("Hub port ID passed {HUBPORT}", "HUBPORT", config.hubport);
+    if (config.hubport)
+    {
+        // platform-1e6a3000.usb-usb-0:1.2:1.0-port0
+        // platform-1e6a3000.usb-usb-0:1.1:1.0-port0
+        spattern = std::format(
+            "platform-{}\\.usb-usb-[0-9]:[0-9]\\.{}:[0-9]\\.{}-port{}",
+            config.address, config.hubport, config.interface, config.port);
+    }
+    else
+    {
+        // platform-1e6a1000.usb-usb-0:1:1.0-port0
+        spattern =
+            std::format("platform-{}\\.usb-usb-[0-9]:[0-9]:[0-9]\\.{}-port{}",
+                        config.address, config.interface, config.port);
+    }
+    std::regex pattern(spattern);
+
     fs::path searchDir = "/dev/serial/by-path/";
 
     for (const auto& entry : fs::recursive_directory_iterator(searchDir))
@@ -93,6 +111,7 @@ auto USBPort::getConfig(sdbusplus::async::context& ctx,
 
     config->address = properties.device_address;
     config->port = properties.port;
+    config->hubport = properties.hub_port;
     config->interface = properties.device_interface;
     config->portType = config::PortType::usb;
 
