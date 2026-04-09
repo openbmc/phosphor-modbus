4759427bd14c48f1de1aade7ece48dbf3b11450b
Jagpal Singh Gill <jagpalgill@fb.com>
2026-04-09 11:40:44 -0700

    rtu: device: add PowerMonitorModule device type
    Add a new PowerMonitorModule device type to support Panasonic
    BJBPM102A0001, Artesyn 7000433970000, and Delta ECD70000020
    power monitor module devices over Modbus RTU.
    
    Change-Id: I366894f80b0f9b11f1aa17198cabf3f11289f765
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ rtu/device/power_monitor_module.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "base_device.hpp"
+
+#include <unordered_set>
+
+namespace phosphor::modbus::rtu::device
+{
+
+namespace config
+{
+
+struct DeviceFactoryConfig;
+
+} // namespace config
+
+class PowerMonitorModule : public BaseDevice
+{
+  public:
+    explicit PowerMonitorModule(
+        sdbusplus::async::context& ctx, const config::Config& config,
+        PortIntf& serialPort, EventIntf::Events& events);
+
+    static auto getInterfaces() -> std::unordered_set<std::string>;
+
+    static auto getConfig(sdbusplus::async::context& ctx,
+                          const sdbusplus::message::object_path& objectPath,
+                          const std::string& interfaceName)
+        -> sdbusplus::async::task<std::optional<config::DeviceFactoryConfig>>;
+};
+
+} // namespace phosphor::modbus::rtu::device
